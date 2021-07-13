4a14032a5cc523f0cae8b66a946abf0a07fc49c1
Edwin Arrieta <earrieta12@hotmail.com>
2021-07-12 23:57:25 -0400

    Versión 1.0 del popular juego de Snake para C#
    Versión 1.0 del popular juego de Snake para C#, basado en las ideas del gran Hector de Leon.

---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.oPictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPuntos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.oPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // oPictureBox
+            // 
+            this.oPictureBox.Location = new System.Drawing.Point(40, 36);
+            this.oPictureBox.Name = "oPictureBox";
+            this.oPictureBox.Size = new System.Drawing.Size(500, 500);
+            this.oPictureBox.TabIndex = 0;
+            this.oPictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 619);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.Location = new System.Drawing.Point(72, 593);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(18, 20);
+            this.lblPuntos.TabIndex = 4;
+            this.lblPuntos.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 872);
+            this.Controls.Add(this.lblPuntos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.oPictureBox);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = " Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.oPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox oPictureBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPuntos;
+    }
+}
+
---
+++ Snake/Snake/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        Game oGame;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            oGame = new Game(oPictureBox, lblPuntos);
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!oGame.IsLost )
+            {
+
+                oGame.Next();
+                oGame.Show();
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Perdiste Cabezón");
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.S)
+                
+            {
+                oGame.ActualDirection = Game.Direction.Up;
+            }
+            if (e.KeyCode == Keys.A)
+
+            {
+                oGame.ActualDirection = Game.Direction.Left;
+            }
+            if (e.KeyCode == Keys.W)
+
+            {
+                oGame.ActualDirection = Game.Direction.Down;
+            }
+            if (e.KeyCode == Keys.D)
+
+            {
+                oGame.ActualDirection = Game.Direction.Right;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oGame = new Game(oPictureBox, lblPuntos);
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            oGame = new Game(oPictureBox, lblPuntos);
+            timer1.Enabled = true;
+        }
+    }
+}
